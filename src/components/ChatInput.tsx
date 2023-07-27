2dfab5bd4667e5010875e1ef4bae12fd64e07595
Whitley Horn <whitleyblaine@protonmail.com>
2023-07-26 22:56:00 -0400

    Implement async handling of stream reading on success

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -20,12 +20,20 @@ const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
         body: JSON.stringify({ messages: [message] }),
       });
 
-      const data = await response.json();
-
-      return data;
+      return response.body;
     },
-    onSuccess: () => {
-      console.log("success");
+    onSuccess: async (stream) => {
+      if (!stream) throw new Error("No stream found");
+      const reader = stream.getReader();
+      const decoder = new TextDecoder();
+      let done = false;
+
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+        const chunkValue = decoder.decode(value);
+        console.log(chunkValue);
+      }
     },
   });
 
